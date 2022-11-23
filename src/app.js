56b0084e26dca7dbb392a5cf217fdcb597328449
yairmc <yairmc666@gmail.com>
2022-11-23 03:06:58 -0700

    changes port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { createUser, getAllUsers } from "./db/userModel.js";
 import { sequelize} from "./db/connection.js";
-import{PORT} from './config.js'
 
 const app = express();
 app.use(cors());
@@ -35,6 +34,7 @@ app.post("/login", async (req, res) => {
 })
 
 app.post("/signUp", async (req, res) => {
+  console.log("HERE");
   const { body } = req;
   try {
     await createUser(body);
@@ -46,4 +46,4 @@ app.post("/signUp", async (req, res) => {
 
 
 
-app.listen(PORT)
+app.listen(3000)
