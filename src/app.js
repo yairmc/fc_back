168f8e0f934de7d079a9654de6ee251a0e0a9d15
yairmc <yairmc666@gmail.com>
2022-11-23 11:43:14 -0700

    changes port

--- src/app.js
+++ src/app.js
@@ -44,6 +44,7 @@ app.post("/signUp", async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 5001;
 
+const PORT = process.env.PORT || 5001
 app.listen(PORT)
+console.log(`server runing in port ${PORT}`);
