1cdd7daf4b28c63aae3f432c4f7057ebdb1d57b6
yairmc <yairmc666@gmail.com>
2022-12-01 15:46:32 -0700

    litle changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { createUser, getAllUsers } from "./db/userModel.js";
-import {createMail, getAllMails} from "./db/mailModel.js"
+import { createMail, getAllMails } from "./db/mailModel.js"
 import { sequelize } from "./db/connection.js";
 
 const app = express();
@@ -30,7 +30,7 @@ app.post("/login", async (req, res) => {
   for (let i = 0; i < users.length; i++) {
     if (users[i].username === username && users[i].password === password) {
       authenticate = true
-      user=users[i];
+      user = users[i];
     }
   }
 
@@ -50,12 +50,31 @@ app.post("/signUp", async (req, res) => {
   }
 })
 
-app.get("/users", async(req,res)=>{
-  try{
-    const users=await getAllUsers();
+app.get("/users", async (req, res) => {
+  try {
+    const users = await getAllUsers();
     res.status(200).json(users)
-  }catch(error){
+  } catch (error) {
     res.status(404).json("Error to get users")
+  }
+})
+
+app.post("/mail", async (req, res) => {
+  const { body } = req;
+  try {
+    await createMail(body);
+    res.status(200).json("Message Send")
+  } catch (error) {
+    res.status(400).json("error to send Message")
+  }
+})
+
+app.get("/mail", async (req,res)=>{
+  try {
+    const mails=await getAllMails();
+    res.status(200).json(mails)
+  } catch (error) {
+    res.status(404).json("error to get mails")
   }
 })
 
